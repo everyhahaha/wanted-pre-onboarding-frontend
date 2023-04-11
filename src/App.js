14e1ff6d060e8d17efac7e1f095aa8e90ae43dea
kimjaewon <everyhahaha@gmail.com>
2023-04-11 12:52:16 +0900

    깃 세팅

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: #c9c9c9;
+  }
+`;
+
+const TodolistBg = styled.div`
+  width: 50%;
+  height: 700px;
+  top: 4rem;
+  left: 16rem;
+  text-align: center;
+  position: absolute;
+  border-radius: 1rem;
+  box-shadow: 0 8px 10px 2px rgba(0,0,0,0.2);
+  background-color: white;
+`;
+
+function Todolist() {
+  return ( 
+    <TodolistBg>
+      <h1>TodoList</h1>
+    </TodolistBg>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+        <Todolist>
+
+        </Todolist>
+    </>
   );
 }
 
